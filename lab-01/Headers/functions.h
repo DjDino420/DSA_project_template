e58f5fa730f1c4bb3cd41b9c31808f67ab8ad4b5
Bambo Peter <bmb.petyx@gmail.com>
2024-02-20 11:32:08 +0200

    DSAI1

---
+++ lab-01/Headers/functions.h
@@ -0,0 +1,9 @@
+//
+// Created by user on 2/20/2024.
+//
+
+#ifndef CPPFELADAT_FUNCTIONS_H
+#define CPPFELADAT_FUNCTIONS_H
+int sum(int a, int b);
+float mini(float a,float b, float c);
+#endif //CPPFELADAT_FUNCTIONS_H
